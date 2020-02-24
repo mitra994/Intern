93a8c3848306e6a065dfea6a50dcef19bb6bebf2
mitra994 <61159037+mitra994@users.noreply.github.com>
2020-02-24 05:08:43 +0100

    Update SilverDiscountCard.java

--- MarketStoreConsole/src/com/MarketStore/Data/DiscountCards/SilverDiscountCard.java
+++ MarketStoreConsole/src/com/MarketStore/Data/DiscountCards/SilverDiscountCard.java
@@ -6,20 +6,32 @@ public class SilverDiscountCard extends DiscountCard{
 
     private final String TYPE = "SILVER";
 
-    public SilverDiscountCard(Customer cardOwner, double lastMonthTurnover){
-        super(cardOwner,lastMonthTurnover);
+    public SilverDiscountCard(Customer cardOwner){
+        super(cardOwner);
+        super.setLastMonthTurnover(0);
 
-         if (lastMonthTurnover > 300){
-            setDiscountRate(3.5);
-        }else{
-            setDiscountRate(2);
-        }
 
     }
-
 
     @Override
     public String getCardType() {
         return TYPE;
+    }
+
+
+    @Override
+    protected void calculateDiscountRate(){
+
+        if (getLastMonthTurnover() > 300){
+            setDiscountRate(3.5);
+        }else{
+            setDiscountRate(2);
+        }
+    }
+    @Override
+    public void setTurnover(double turnover){
+        super.setLastMonthTurnover(turnover);
+        calculateDiscountRate();
+
     }
 }
