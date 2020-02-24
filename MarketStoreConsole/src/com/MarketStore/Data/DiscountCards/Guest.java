85a038d61991d7684fb9d5ab6d9a60ee5cf82e2c
mitra994 <61159037+mitra994@users.noreply.github.com>
2020-02-24 05:09:13 +0100

    Update Guest.java

--- MarketStoreConsole/src/com/MarketStore/Data/DiscountCards/Guest.java
+++ MarketStoreConsole/src/com/MarketStore/Data/DiscountCards/Guest.java
@@ -1,19 +1,1 @@
-package com.MarketStore.Data.DiscountCards;
 
-public class Guest extends  DiscountCard {
-
-    private final String TYPE = "GUEST";
-
-    public Guest(){
-        setDiscountRate(0);
-        setLastMonthTurnover(0);
-
-    }
-
-    @Override
-    public String getCardType() {
-        return this.TYPE;
-    }
-
-
-}
